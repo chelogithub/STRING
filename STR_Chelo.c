69e21519b3013a3a8156093779539e06ee6e7232
Marcelo García <43250973+chelogithub@users.noreply.github.com>
2025-05-22 07:58:10 +0200

    250522
    Nothing

--- STR_Chelo.c
+++ STR_Chelo.c
@@ -547,7 +547,7 @@ if (c==0) x=strlen(b);
 else
 	{
 		x=c;
-		if (x>1023)// OJO SOLO HTTP if (x>128) (x>512)
+		if (x>4096)//(x>1023)// OJO SOLO HTTP if (x>128) (x>512)
 			{
 				return(1);  //Estoy pasado de cantidad de datos
 			}
